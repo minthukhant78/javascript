5a1fd703454c30cb4518a79e2ce26dfb0364f281
minthukhant78 <minthukhant2001@outlook.com>
2024-06-10 16:23:38 +0630

    day-1

---
+++ 1-javascriptbasice.js
@@ -0,0 +1,7 @@
+// What is JavaScript 
+// go to website supersimple.dev/js-basics
+// website ကိုသွား စမ်းကြည့်ပါ အောက်ပါအတိုင်း
+// alert('Hello');
+// 2+2=
+
+document.body.innerHTML = 'Hello';
---
+++ 3.string.js
@@ -0,0 +1,9 @@
+// string = text
+// typeof 'text' {string}
+// typeof 2 {number}
+
+
+// '&' + (83+97)
+// '&180'
+
+ 
