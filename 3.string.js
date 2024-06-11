31f200f7c02caaa904a5e7ef8b14366f8f3f9e3c
minthukhant78 <minthukhant2001@outlook.com>
2024-06-11 15:53:44 +0630

    day-2

--- 3.string.js
+++ 3.string.js
@@ -6,4 +6,8 @@
 // '&' + (83+97)
 // '&180'
 
- 
+//  \" = double quote that is just text
+//  \n = newline character
+//  `...` = template string
+// အောက်ပါရေးထား  Line nunber 13  ကို Concatenation လို့ခေါ်ပ်တယ်။
+'Items (' + (1+ 1) + '):$' +(2095 + 799) / 100
